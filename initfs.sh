905234450b23fae2a8297d087fbf4cc7b760b6f1
Norman Zhi <normanzyb@gmail.com>
2023-08-27 00:05:51 +0100

    Add QThread support to Qt2 in initfs/loaderfs

--- initfs.sh
+++ initfs.sh
@@ -6,7 +6,7 @@ mkdir -p rootfs_$img
 umount -l rootfs_$img
 
 set -e
-dd if=/dev/zero of=$img bs=40M count=1
+dd if=/dev/zero of=$img bs=48M count=1
 mke2fs -F $img
 mount -t ext2 -o rw $img rootfs_$img
 cp -a $base/* rootfs_$img/
